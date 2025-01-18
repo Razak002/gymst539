7229867041504a4b4ab3deb5d025e20ab1deb6c4
Aliyu Abdulrazak <106888664+Razak002@users.noreply.github.com>
2025-01-18 12:54:40 +0100

    fixes:responsiveness fixes

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -5,7 +5,7 @@ import React from 'react'
         <div id='main'>
             <div className='pr-heading'>
                 <h2>STEP UP YOUR</h2>
-                <h1><span>FITNESS</span>WITH US</h1>
+                <h2><span>FITNESS  </span>NOW</h2>
                 <p className='details'>Buils Your Body And Fitness With Professional Touch</p>
                 <div className='header-btns'>
                     <a href='#' className='header-btn'>JOIN US</a>
